f7214fe2457cc3daa9e7de3a1ae353a2c2174b53
viktor <vicmarmartinezmartinez@gmail.com>
2023-10-19 13:32:11 +0200

    php-trabajo

---
+++ iaw/php/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 php</title>
+</head>
+<body>
+    <?php
+        echo ("<pre>");
+        print_r($_SERVER['REMOTE_ADDR']);
+        #echo "Direccion IP del servidor:" . $_SERVER['SERVER_ADDR'] . 
+        #echo "Nombre del HOST del servidor:"
+        #echo "Software usado del servidor:"
+        #echo "Info sobre el usuario que esta usando el script:"
+        #echo "Direccion IP del cliente:"    
+    ?>
+</body>
+</html>
